17159e608e835e32a54a9af9e59a014c64d912c0
iam-krishnavamsi <109576412+iam-krishnavamsi@users.noreply.github.com>
2026-05-27 17:27:18 +0530

    Update build.grade.kts

--- build.grade.kts
+++ build.grade.kts
@@ -1,20 +1,7 @@
 plugins {
     kotlin("jvm") version "1.9.24"
-    application
 }
 
 repositories {
     mavenCentral()
-}
-
-dependencies {
-    testImplementation(kotlin("test"))
-}
-
-application {
-    mainClass.set("AppKt")
-}
-
-tasks.test {
-    useJUnitPlatform()
 }
