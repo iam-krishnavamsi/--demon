77fa5c3caee16a2993d22e6fbcb9650347a650bf
iam-krishnavamsi <109576412+iam-krishnavamsi@users.noreply.github.com>
2026-05-27 17:14:19 +0530

    Initialize build.gradle.kts with Kotlin setup

---
+++ build.grade.kts
@@ -0,0 +1,15 @@
+plugins {
+    kotlin("jvm") version "1.9.24"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
